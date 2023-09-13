baa336a7e351fa6d9c12a614930caddc903e01d0
CleberAGallo <cleber.gallo@etec.sp.gov.br>
2023-09-12 22:11:47 -0300

    12/09

---
+++ LojaServer/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+using LojaServer.Collections;
+namespace LojaServer.Repositories;
+public interface IProductRepository
+{
+    Task<List<Product>> GetAllAsync();
+    Task<Product> GetByIdAsync(string id);
+    Task CreateAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task DeleteAsync(string id);
+}
